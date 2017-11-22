f37b8dfcc72cd456f039ae7414e5f90edeb94374
Macrob <epishkin.serg@gmail.com>
2017-11-22 21:07:40 +0200

    packages updates
    packages updates

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -35,7 +35,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/macrob/ng.git',
       path : '/var/www/sms',
-      'post-deploy' : 'cd server && npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'cd server; npm install && pm2 start ecosystem.config.js WEB --env production'
     },
     dev : {
       user : 'node',
