f56a149856c99bae28483bbce292cdaabd76c912
Macrob <epishkin.serg@gmail.com>
2017-11-25 10:49:46 +0200

    ff
    fff

--- server/src/models/connections.ts
+++ server/src/models/connections.ts
@@ -1,5 +1,4 @@
-import { mysql } from '../config/config';
-
 import { getConnection } from '../express/express';
+import { mysql } from '../config/config';
 console.log(mysql);
 export const connection = getConnection(mysql.test);
