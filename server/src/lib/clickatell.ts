252e1534ebf98b2b2027e577a7a119486c721b9d
Macrob <epishkin.serg@gmail.com>
2017-11-25 09:19:19 +0200

    fix status sms
    fix status sms

--- server/src/lib/clickatell.ts
+++ server/src/lib/clickatell.ts
@@ -6,11 +6,11 @@ export function send(sms: {phone: string; text: string}) {
       user: 'prapor',
       pass: 'praportest1',
       api: '3642879',
-      // to: sms.phone,
-      to: '380632519584',
+      to: _.replace(sms.phone, '+', ''),
+      // to: '380632519584',
       msg: sms.text
     } 
-
+    console.log(params);
     let clickRequest = _.template('http://api.clickatell.com/http/sendmsg?user=<%=user%>&password=<%=pass%>&api_id=<%=api%>&to=<%=to%>&text=<%=msg%>');
 
     return (async function() {
