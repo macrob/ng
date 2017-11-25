3b39572ba8c25c54f6b5ee29e10deecd7b4565d9
Macrob <epishkin.serg@gmail.com>
2017-11-25 14:10:04 +0200

    syncdb
    syncdb

--- server/src/models/models.ts
+++ server/src/models/models.ts
@@ -1,3 +1,6 @@
 export * from './sms';
 export * from './smsItems';
 export * from './user';
+
+import { connection as sequelize} from './connections';
+sequelize.sync().then(console.log).catch(console.error);
